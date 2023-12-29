a845755c4905fa77c697b19437ceac80d08e1433
peter <abanoub.samy.mikhail@gmail.com>
2023-12-29 16:46:54 +0200

    fix carousel-home-page

--- src/app/home-page/home-page.module.ts
+++ src/app/home-page/home-page.module.ts
@@ -3,15 +3,18 @@ import { CommonModule } from '@angular/common';
 import { SlideComponent } from './components/slide/slide.component';
 import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
     SlideComponent,
+    CardComponent
   ],
   imports: [
     CommonModule,
     RouterModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    
   ]
 })
 export class HomePageModule { }
