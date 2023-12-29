f4ac78e4cd3e9690d8522a88f5c272f39caa09e6
hamza-halawa2020 <h.halawa2020@gmail.com>
2023-12-29 23:54:38 +0200

    sign in page to arabic

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,16 +3,10 @@ import { CommonModule } from '@angular/common';
 import { FooterComponent } from './components/footer/footer.component';
 import { SpinnerComponent } from './components/spinner/spinner.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
-
-
+import { RouterLink } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    SpinnerComponent,
-    NotFoundComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [SpinnerComponent, NotFoundComponent],
+  imports: [CommonModule, RouterLink],
 })
-export class SharedModule { }
+export class SharedModule {}
